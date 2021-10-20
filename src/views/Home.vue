0c675e9d92857228daa54a4b346b0c02081cba1c
backendpapa <akejupaul10@gmail.com>
2021-10-20 23:55:11 +0100

    make it

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
       <v-btn text class="text-caption font-weight-bold d-none d-sm-flex " to="/learn" color="white" plain><span class="mr-2">&#183;</span>TUTORIALS</v-btn>
       
       <v-spacer></v-spacer>
-      <v-btn outlined color="white" rounded dark @click="drawer=!drawer" class="mr-4  d-none d-sm-flex" style="text-transform:none" plain> Login</v-btn>
+      <v-btn outlined color="white" rounded dark @click="drawer=!drawer" class="mr-4 text-caption  d-none d-sm-flex" style="text-transform:none" large plain> Access Bucket <v-icon>mdi-chevron-right</v-icon></v-btn>
       <v-app-bar-nav-icon class="white--text mt-n4  d-flex d-sm-none" ></v-app-bar-nav-icon>
         </v-row>
       </v-container>
@@ -44,7 +44,7 @@
       <v-text-field dense outlined  label="Region" v-model="region">
 
       </v-text-field>
-      <v-btn block :loading="loading"  @click="login" color="black" dark>Login</v-btn>
+      <v-btn block :loading="loading" style="text-transform:none"  @click="login" color="black" dark>Access Bucket <v-icon>mdi-chevron-right</v-icon></v-btn>
     </div>
      
     </v-navigation-drawer>
@@ -62,7 +62,7 @@
         <div style="height:100%;width:100%" class="animate__animated animate__fadeInDown">
         <p class="text-h1 font-weight-bold white--text  ">S3 Bucket Closer Than Ever</p>
         
-        <v-btn rounded @click="drawer=!drawer" color="white" style="text-transform:none" class="text-body-2 blue--text animate__animated animate__fadeIn animate__slow animate__delay-1s" x-large>Access S3 bucket <v-icon class="ml-4">mdi-chevron-right</v-icon></v-btn>
+        <v-btn rounded @click="drawer=!drawer" color="white" style="text-transform:none" class="text-body-2 blue--text animate__animated animate__fadeIn animate__slow animate__delay-1s" x-large>Access S3 Bucket <v-icon class="ml-4">mdi-chevron-right</v-icon></v-btn>
       </div> 
       </v-col>
       <v-col cols="12" xl="5" lg="5"></v-col>
@@ -83,6 +83,116 @@
 
 
        
+      </v-row>
+
+      <div>
+        <p class="text-h3 text-center font-weight-bold white--text">Advantages</p>
+
+      </div>
+
+      <!-- Advantages Desktop -->
+      <v-row class="d-none d-sm-flex" no-gutters>
+        <v-col cols="2" />
+        <v-col cols="8">
+          <v-row no-gutters align="center" justify="center">
+            <v-col cols="4" class="mr-n1">
+              <v-card style="height:50vh;border:1px #96E4F0 solid"  color="#3B6DDE" hover>
+                <v-row style="height:100%" no-gutters align="center" justify="center">
+                  <v-spacer></v-spacer>
+                  <div class="" style="margin:20px">
+                        <p class="text-center"><v-icon size="140" color="white" style="opacity:0.1">mdi-circle-outline</v-icon></p>
+                        <p class="text-h5 font-weight-bold white--text  text-center ">Instant  S3 Access</p>
+                        <p class="text-center text-caption " style="color:#96E4F0">Access files and folders in minutes just with your S3 credentials.</p>
+                  </div>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-card>
+            </v-col>
+            <v-col cols="4" style="position:relative;z-index:2">
+              <v-card style="height:60vh" color="#4987FF" hover>
+
+                  <v-row style="height:100%" no-gutters align="center" justify="center">
+                  <v-spacer></v-spacer>
+                  <div class="" style="margin:20px">
+                        <p class="text-center"><v-icon size="140" color="white" style="opacity:0.1">mdi-circle-outline</v-icon></p>
+                        <p class="text-h5 font-weight-bold white--text  text-center ">Privacy</p>
+                        <p class="text-center text-caption " style="color:#96E4F0">Your S3 credentials are only known by you and are not stored or saved on any database.</p>
+                        <p class="text-center"><v-btn class="text-caption" large color="white" style="text-transform:none" rounded>Learn more <v-icon >mdi-chevron-right</v-icon></v-btn></p>
+                  </div>
+                  <v-spacer></v-spacer>
+                </v-row>
+
+                <div class="white" style="height:5px;width:100%;position:absolute;bottom:0">
+
+                </div>
+              </v-card>
+            </v-col>
+            <v-col cols="4" class="ml-n1">
+              <v-card style="height:50vh;border:1px #96E4F0 solid" color="#3B6DDE" hover>
+                <v-row style="height:100%" no-gutters align="center" justify="center">
+                  <v-spacer></v-spacer>
+                  <div class="" style="margin:20px">
+                        <p class="text-center"><v-icon size="140" color="white" style="opacity:0.1">mdi-circle-outline</v-icon></p>
+                        <p class="text-h5 font-weight-bold white--text  text-center ">Interactive</p>
+                        <p class="text-center text-caption " style="color:#96E4F0">Open,Edit and Read documents(PDF,CSV...), play your media files right on the platform without having to download the files.</p>
+                  </div>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="2" />
+      </v-row>
+
+      <!-- Advantages mobile -->
+      <v-row no-gutters class="ma-2  d-flex d-sm-none">
+        <v-col cols="12">
+            <v-card style="height:50vh;border:1px #96E4F0 solid" class="mb-2"  color="#3B6DDE" hover>
+                <v-row style="height:100%" no-gutters align="center" justify="center">
+                  <v-spacer></v-spacer>
+                  <div class="" style="margin:20px">
+                        <p class="text-center"><v-icon size="140" color="white" style="opacity:0.1">mdi-circle-outline</v-icon></p>
+                        <p class="text-h5 font-weight-bold white--text  text-center ">Instant  S3 Access</p>
+                        <p class="text-center text-caption " style="color:#96E4F0">Access files and folders in minutes just with your S3 credentials.</p>
+                  </div>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card style="height:60vh" color="#4987FF" class="mb-2" hover>
+
+                  <v-row style="height:100%" no-gutters align="center" justify="center">
+                  <v-spacer></v-spacer>
+                  <div class="" style="margin:20px">
+                        <p class="text-center"><v-icon size="140" color="white" style="opacity:0.1">mdi-circle-outline</v-icon></p>
+                        <p class="text-h5 font-weight-bold white--text  text-center ">Privacy</p>
+                        <p class="text-center text-caption " style="color:#96E4F0">Your S3 credentials are only known by you and are not stored or saved on any database.</p>
+                        <p class="text-center"><v-btn class="text-caption" large color="white" style="text-transform:none" rounded>Learn more <v-icon >mdi-chevron-right</v-icon></v-btn></p>
+                  </div>
+                  <v-spacer></v-spacer>
+                </v-row>
+
+                <div class="white" style="height:5px;width:100%;position:absolute;bottom:0">
+
+                </div>
+              </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card style="height:50vh;border:1px #96E4F0 solid" color="#3B6DDE" hover>
+                <v-row style="height:100%" no-gutters align="center" justify="center">
+                  <v-spacer></v-spacer>
+                  <div class="" style="margin:20px">
+                        <p class="text-center"><v-icon size="140" color="white" style="opacity:0.1">mdi-circle-outline</v-icon></p>
+                        <p class="text-h5 font-weight-bold white--text  text-center ">Interactive</p>
+                        <p class="text-center text-caption " style="color:#96E4F0">Open,Edit and Read documents(PDF,CSV...), play your media files right on the platform without having to download the files.</p>
+                  </div>
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-card>
+        </v-col>
+
       </v-row>
     </v-container>
     
